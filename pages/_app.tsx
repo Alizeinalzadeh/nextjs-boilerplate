6a4e821e861276942eaf2784fa523ca1fa2821f5
ali.zeinalzadeh <ali.zeinalzadeh@chapagha.com>
2022-07-20 17:03:06 +0430

    add generate component CLI to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
-import { SEO_CONFIG } from '@utils/config';
+import { SEO_CONFIG } from '@utils/Config';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
