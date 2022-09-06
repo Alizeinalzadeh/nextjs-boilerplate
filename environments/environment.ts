f68eb6ac24f8e04f90b075ec3c52f87f46e75342
ali.zeinalzadeh <ali.zeinalzadeh@chapagha.com>
2022-09-06 17:19:01 +0430

    [STRUCTURE]: make env variables globally accessable

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
-	isProduction: process.env.NODE_ENV == 'production' ? true : false,
-	env: process.env.NODE_ENV,
-	APP_DEBUG: process.env.APP_DEBUG,
+	isProduction: process.env.NEXT_PUBLIC_NODE_ENV == 'production' ? true : false,
+	env: process.env.NEXT_PUBLIC_NODE_ENV,
+	APP_DEBUG: process.env.NEXT_PUBLIC_APP_DEBUG,
 	api: {
 		www: {
-			baseUrl: process.env.BASE_URL,
-			baseEndpoint: process.env.BASE_ENDPOINT,
+			baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+			baseEndpoint: process.env.NEXT_PUBLIC_BASE_ENDPOINT,
 		},
 	},
 	file: {
-		serverFileHost: process.env.SERVER_FILE_HOST,
-		serverFileCDNHost: process.env.SERVER_CDN_HOST,
+		serverFileHost: process.env.NEXT_PUBLIC_SERVER_FILE_HOST,
+		serverFileCDNHost: process.env.NEXT_PUBLIC_SERVER_CDN_HOST,
 	},
 };
